4db1da41d48f52360cef7d8948857b3b233f4650
NiharikaChhabra700 <72100217+NiharikaChhabra700@users.noreply.github.com>
2022-01-08 15:17:46 +0530

    Time: 2 ms (84.55%), Space: 39.3 MB (20.56%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -28,45 +28,45 @@ class Solution {
     }
     
     
-//     public boolean sol(String s1,String s2,String s3,int i, int j,Boolean[][] dp)
-//     {
-//         if(i==s1.length() && j==s2.length())
-//         {
-//             return dp[i][j]=true;
-//         }
+    public boolean sol(String s1,String s2,String s3,int i, int j,Boolean[][] dp)
+    {
+        if(i==s1.length() && j==s2.length())
+        {
+            return dp[i][j]=true;
+        }
         
-//         if(dp[i][j] !=null) return dp[i][j];
+        if(dp[i][j] !=null) return dp[i][j];
         
-//         if(i<s1.length() && s1.charAt(i)==s3.charAt(i+j))
-//         {
-//             boolean f1=sol(s1,s2,s3,i+1,j,dp);
-//             dp[i][j]=f1;
+        if(i<s1.length() && s1.charAt(i)==s3.charAt(i+j))
+        {
+            Boolean f1=sol(s1,s2,s3,i+1,j,dp);
+            dp[i][j]=f1;
             
-//             if(f1==true)
-//             {
-//                 return true;
-//             }
-//         }
+            if(f1==true)
+            {
+                return true;
+            }
+        }
         
-//         if(j<s2.length() && s2.charAt(j)==s3.charAt(i+j))
-//         {
-//             boolean f2=sol(s1,s2,s3,i,j+1,dp);
-//             dp[i][j]=f2;
-//             if(f2)
-//             {
-//                 return true;
-//             }
-//         }
+        if(j<s2.length() && s2.charAt(j)==s3.charAt(i+j))
+        {
+            Boolean f2=sol(s1,s2,s3,i,j+1,dp);
+            dp[i][j]=f2;
+            if(f2)
+            {
+                return true;
+            }
+        }
         
-//         dp[i][j]=false;
-//         return false;
-//     }
+        dp[i][j]=false;
+        return false;
+    }
     
     public boolean isInterleave(String s1, String s2, String s3) {
         if(s1.length()+s2.length()!=s3.length())
          return false;
         
-        return solve(s1,s2,s3,0,0,new Boolean[s1.length()+1][s2.length()+1]);
+        return sol(s1,s2,s3,0,0,new Boolean[s1.length()+1][s2.length()+1]);
         
     }
 }
