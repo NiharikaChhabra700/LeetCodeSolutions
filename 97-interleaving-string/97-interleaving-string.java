c240d27032bd5ae084742e0568534a0b7110bc83
NiharikaChhabra700 <72100217+NiharikaChhabra700@users.noreply.github.com>
2022-01-08 15:25:52 +0530

    Time: 6 ms (39.65%), Space: 37.4 MB (67.79%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,72 +1,45 @@
 class Solution {
-    
-    public Boolean solve(String s1, String s2, String s3,int i,int j,Boolean dp[][])
-    {
-     
-     if(i==s1.length()&&j==s2.length())
-         return true;//perfectly matched if got till here
-        
-     if(dp[i][j]!=null)
-         return dp[i][j];
-        
-     if(i<s1.length()&&s1.charAt(i)==s3.charAt(i+j))
-     {
-         dp[i][j]=solve(s1,s2,s3,i+1,j,dp);
-         if(dp[i][j]==true)//directly return mat krna ,next ko bhi check 
-             return dp[i][j];
-     }
-        
-     if(j<s2.length()&&s2.charAt(j)==s3.charAt(i+j))
-     {
-         dp[i][j]=solve(s1,s2,s3,i,j+1,dp);
-         if(dp[i][j]==true)
-             return dp[i][j];
-     }
-     
-     dp[i][j]=false;
-     return dp[i][j];
-    }
-    
-    
-    public boolean sol(String s1,String s2,String s3,int i, int j,Boolean[][] dp)
-    {
-        if(i==s1.length() && j==s2.length())
-        {
-            return dp[i][j]=true;
-        }
-        
-        if(dp[i][j] !=null) return dp[i][j];
-        
-        if(i<s1.length() && s1.charAt(i)==s3.charAt(i+j))
-        {
-            Boolean f1=sol(s1,s2,s3,i+1,j,dp);
-            dp[i][j]=f1;
-            
-            if(f1==true)
-            {
-                return true;
-            }
-        }
-        
-        if(j<s2.length() && s2.charAt(j)==s3.charAt(i+j))
-        {
-            Boolean f2=sol(s1,s2,s3,i,j+1,dp);
-            dp[i][j]=f2;
-            if(f2)
-            {
-                return true;
-            }
-        }
-        
-        dp[i][j]=false;
-        return false;
-    }
-    
     public boolean isInterleave(String s1, String s2, String s3) {
+        boolean[][] dp=new boolean[s1.length()+1][s2.length()+1];
+        
         if(s1.length()+s2.length()!=s3.length())
-         return false;
+        {
+            return false;
+        }
         
-        return sol(s1,s2,s3,0,0,new Boolean[s1.length()+1][s2.length()+1]);
+        for(int i=0;i<=s1.length();i++)
+        {
+            for(int j=0;j<=s2.length();j++ )
+            {
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=true;
+                }
+                
+                else if(i==0)
+                {
+                    dp[i][j]= (s3.charAt(i+j-1)==s2.charAt(j-1)) ? dp[i][j-1] : false; 
+                }
+                else if(j==0)
+                {
+                   dp[i][j]= s3.charAt(i+j-1)==s1.charAt(i-1) ? dp[i-1][j] : false; 
+                }
+                else{
+                    
+                    if(s1.charAt(i-1)==s3.charAt(i+j-1))
+                    {
+                        dp[i][j]=dp[i-1][j];
+                    }
+                    
+                    if(!dp[i][j] && s2.charAt(j-1)==s3.charAt(i+j-1))
+                    {
+                        dp[i][j]=dp[i][j-1];
+                    }
+                    
+                }
+            }
+        }
         
+        return dp[s1.length()][s2.length()];
     }
 }
