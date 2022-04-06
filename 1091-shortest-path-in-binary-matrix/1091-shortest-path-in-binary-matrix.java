0fb39d702dfbb6529ad2e53cf41789dc1b3b89f7
NiharikaChhabra700 <72100217+NiharikaChhabra700@users.noreply.github.com>
2022-04-06 11:14:13 +0530

    Time: 26 ms (54.52%), Space: 58.3 MB (70.89%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,5 +1,6 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
+        
         if(grid.length==0 || grid[0].length==0)
         {
             return -1;
@@ -8,49 +9,51 @@ class Solution {
         int n=grid.length;
         int m=grid[0].length;
         
-        if(grid[0][0]== 1 || grid[n-1][m-1]==1)
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1)
         {
             return -1;
+            
         }
         
-        int[][] dir={{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,1},{-1,-1},{1,-1}};
+         int[][] dir={{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,1},{-1,-1},{1,-1}};
+        
         
         LinkedList<Integer> que=new LinkedList<>();
-        
         que.addLast(0);
-        int shortestpath=1;
+        int level=1;
         
         while(que.size()!=0)
         {
             int size=que.size();
             
-            while(size-->0)
+            while(size-- > 0)
             {
                 int idx=que.removeFirst();
                 
                 int r=idx/m;
                 int c=idx%m;
                 
-                if(r==n-1 && c==m-1)
+                if(r == n-1 && c == m-1)
                 {
-                    return shortestpath;
+                    return level;
                 }
                 
-                for(int[] d: dir)
+                for(int d=0;d<dir.length;d++)
                 {
-                    int x= r+d[0];
-                    int y=c+ d[1];
+                    int x=r+dir[d][0];
+                    int y=c+dir[d][1];
                     
-                    if(x>=0 && y>=0 && x<n && y<m && grid[x][y]==0)
+                    if(x>=0 && y>=0 && x<n && y<m && grid[x][y] == 0)
                     {
                         grid[x][y]=1;
                         que.addLast(x*m+y);
                     }
-        
                 }
+                
+                
             }
             
-            shortestpath++;
+            level++;
         }
         
         return -1;
