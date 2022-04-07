127e3d82d0a115f2a00d0803b68f5a7d63dcacc0
NiharikaChhabra700 <72100217+NiharikaChhabra700@users.noreply.github.com>
2022-04-07 21:28:29 +0530

    Time: 5 ms (81.31%), Space: 65.9 MB (36.24%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,53 +1,68 @@
 class Solution {
-
-    public void dfs(int[][] grid,int i ,int j,int[][] dir)
+    
+     public void dfs(int[][] board,int i,int j,int[][] dir)
     {
-        grid[i][j]=0;
+        board[i][j]=0;
         
         for(int d=0;d<dir.length;d++)
         {
-            int x=i+dir[d][0];
-            int y=j+dir[d][1];
+            int x= i + dir[d][0];
+            int y= j + dir[d][1];
             
-            if(x>=0 && y>=0 && x<grid.length && y<grid[0].length && grid[x][y]==1)
+            if(x>=0 && y>=0 && x<board.length && y<board[0].length && board[x][y]==1)
             {
-                dfs(grid,x,y,dir);
+                dfs(board,x,y,dir);
             }
         }
     }
     
     
-    
-    public int numEnclaves(int[][] grid) {
-        if(grid.length==0 || grid[0].length==0 )
+    public int numEnclaves(int[][] board) {
+        if(board.length==0 || board[0].length==0)
         {
             return 0;
         }
         
-        int[][] dir={{1,0},{-1,0},{0,1},{0,-1}};
+        int[][] dir={{-1,0},{1,0},{0,1},{0,-1}};
+        int n=board.length;
+        int m=board[0].length;
         
-        for(int i=0;i<grid.length;i++)
+        
+        for(int i=0;i<board.length;i++)
         {
-            for(int j=0;j<grid[0].length;j++)
+            if(board[i][0]==1)
             {
-                if(i==0 || j==0 || i==grid.length-1 || j==grid[0].length-1)
-                {
-                    if(grid[i][j]==1)
-                    {
-                        dfs(grid,i,j,dir);
-                    }
-                }
+                dfs(board,i,0,dir);
             }
+            if(board[i][m-1]==1)
+            {
+                dfs(board,i,m-1,dir);
+            }
+            
         }
         
         
+        
+        for(int j=0;j<board[0].length;j++)
+        {
+            if(board[0][j]==1)
+            {
+                dfs(board,0,j,dir);
+                
+            }
+            if(board[n-1][j]==1)
+            {
+                dfs(board,n-1,j,dir);
+            }
+        }
+        
         int count=0;
         
-        for(int i=0;i<grid.length;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<grid[0].length;j++)
+            for(int j=0;j<m;j++)
             {
-                if(grid[i][j]==1)
+                if(board[i][j]==1)
                 {
                     count++;
                 }
@@ -55,6 +70,7 @@ class Solution {
         }
         
         return count;
+        
         
     }
 }
