d71802114c8dfe95167fe3c723c63efaaeb9b8ee
NiharikaChhabra700 <72100217+NiharikaChhabra700@users.noreply.github.com>
2022-04-07 19:45:48 +0530

    Time: 6 ms (72.75%), Space: 47.9 MB (60.01%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -2,48 +2,51 @@ class Solution {
     
     public boolean kahns(ArrayList<Integer>[] graph,int n)
     {
-        int[] indegree=new int[n];
+       int[] indegree=new int[n];
         
-        for(ArrayList<Integer> arr: graph)
+        for(ArrayList<Integer> arr : graph)
         {
-            for(int v: arr)
+            for(int i : arr)
             {
-                indegree[v]++;
+                indegree[i]++;
             }
         }
         
         LinkedList<Integer> que=new LinkedList<>();
-        ArrayList<Integer> ans=new ArrayList<>();
         
         for(int i=0;i<n;i++)
         {
-            if(indegree[i]==0) que.addLast(i);
+            if(indegree[i]==0)
+            {
+                que.addLast(i);
+            }
         }
+        
+        ArrayList<Integer> ans=new ArrayList<>();
         
         while(que.size()!=0)
         {
+            
             int size=que.size();
+            
             while(size-->0)
             {
                 int rvtx=que.removeFirst();
                 ans.add(rvtx);
                 
-                for(int v: graph[rvtx])
+                for(int v : graph[rvtx])
                 {
                     if(--indegree[v]==0)
                     {
                         que.addLast(v);
                     }
                 }
+                
             }
-            
         }
         
         return ans.size()==n;
-        
-        
     }
-    
     
     
     
@@ -57,7 +60,7 @@ class Solution {
             graph[i]=new ArrayList<>();
         }
         
-        for(int[] arr:prerequisites)
+        for(int[] arr: prerequisites)
         {
             graph[arr[0]].add(arr[1]);
         }
