4711dbbb21c505847958bda44743b6a43fb6c40c
NiharikaChhabra700 <72100217+NiharikaChhabra700@users.noreply.github.com>
2022-04-21 17:12:07 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])throws IOException
+    {
+        //reading input using BufferedReader class
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //reading total testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            //reading number of elements and weight
+            int n = Integer.parseInt(read.readLine());
+            int w = Integer.parseInt(read.readLine());
+            
+            int val[] = new int[n];
+            int wt[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            //inserting the values
+            for(int i = 0; i < n; i++)
+              val[i] = Integer.parseInt(st[i]);
+             
+            String s[] = read.readLine().trim().split("\\s+"); 
+            
+            //inserting the weigths
+            for(int i = 0; i < n; i++)
+              wt[i] = Integer.parseInt(s[i]);
+              
+            //calling method knapSack() of class Knapsack
+            System.out.println(new Solution().knapSack(w, wt, val, n));
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution 
+{ 
+    //Function to return max value that can be put in knapsack of capacity W.
+    
+    public static int knapsack(int [] weight,int[] val, int n,int bagweight,int[][] dp)
+    {
+        if(n==0 || bagweight==0)
+        {
+            return dp[n][bagweight]=0;
+        }
+        
+        if(dp[n][bagweight]!=-1) return dp[n][bagweight];
+        
+        if(bagweight-weight[n-1]>=0)
+        {
+            dp[n][bagweight]=knapsack(weight,val,n-1,bagweight-weight[n-1],dp)+val[n-1];
+        }
+        
+        dp[n][bagweight]=Math.max(dp[n][bagweight],knapsack(weight,val,n-1,bagweight,dp));
+        
+        return dp[n][bagweight];
+    }
+    
+    
+    
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+         // your code here 
+         int[][] dp=new int[n+1][W+1];
+         for(int[]d:dp) Arrays.fill(d,-1);
+         return knapsack(wt,val,n,W,dp);
+    } 
+}
+
+
