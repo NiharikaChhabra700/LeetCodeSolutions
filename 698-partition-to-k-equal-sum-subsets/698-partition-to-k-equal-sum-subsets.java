2ef28c2a41d704a572411a7be78f6f5112b2dc8e
NiharikaChhabra700 <72100217+NiharikaChhabra700@users.noreply.github.com>
2022-04-21 17:36:49 +0530

    Time: 2030 ms (30.88%), Space: 40.2 MB (75.63%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -0,0 +1,51 @@
+class Solution {
+    
+    public boolean canpartition(int[] arr,int k,int idx,int sumsf,int tar,boolean[] vis)
+    {
+        if(k==0)
+        {
+            return true;
+        }
+        
+        if(sumsf==tar)
+        {
+            return canpartition(arr,k-1,0,0,tar,vis);
+        }
+        
+        
+        boolean res=false;
+        for(int i=idx;i<arr.length;i++)
+        {
+            if(vis[i]) continue;
+            
+            vis[i]=true;
+            res=res || canpartition(arr,k,i+1,sumsf+arr[i],tar,vis);
+            vis[i]=false;
+        }
+        
+        return res;
+    }
+    
+    
+    
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        
+        int sum=0;
+        int maxele=0;
+        
+        for(int ele:nums)
+        {
+            sum+=ele;
+            maxele=Math.max(maxele,ele);
+        }
+        
+        if(sum%k != 0 || maxele >sum/k) return false;
+        
+        int n=nums.length;
+        
+        boolean[] vis=new boolean[n+1];
+        return canpartition(nums,k,0,0,sum/k,vis);
+        
+        
+    }
+}
