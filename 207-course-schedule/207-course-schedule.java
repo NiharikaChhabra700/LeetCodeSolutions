043ef4aed89fe0145a5dfd99956c243302f3e2b3
Niharika Chhabra <72100217+NiharikaChhabra700@users.noreply.github.com>
2022-07-04 00:42:12 +0530

    Time: 9 ms (53.16%), Space: 48.1 MB (55.55%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,71 +1,68 @@
 class Solution {
     
-    public boolean kahns(ArrayList<Integer>[] graph,int n)
+    public boolean Kahns(ArrayList<Integer>[] graph , int n)
     {
-       int[] indegree=new int[n];
-        
-        for(ArrayList<Integer> arr : graph)
-        {
-            for(int i : arr)
-            {
-                indegree[i]++;
-            }
-        }
-        
-        LinkedList<Integer> que=new LinkedList<>();
+        int[] indegree = new int[n];
         
         for(int i=0;i<n;i++)
         {
-            if(indegree[i]==0)
+            for(int e : graph[i])
+            {
+                indegree[e]++;
+            }
+        }
+        
+        LinkedList<Integer> que = new LinkedList<>();
+        
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i] == 0)
             {
                 que.addLast(i);
             }
         }
         
-        ArrayList<Integer> ans=new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
         
         while(que.size()!=0)
         {
-            
             int size=que.size();
-            
-            while(size-->0)
+            while(size-- > 0)
             {
-                int rvtx=que.removeFirst();
+                int rvtx = que.removeFirst();
                 ans.add(rvtx);
                 
-                for(int v : graph[rvtx])
+                for(int e : graph[rvtx])
                 {
-                    if(--indegree[v]==0)
+                    if(--indegree[e] == 0)
                     {
-                        que.addLast(v);
+                        que.addLast(e);
                     }
                 }
-                
             }
         }
         
-        return ans.size()==n;
+        return ans.size() == n;
     }
-    
     
     
     
     public boolean canFinish(int n, int[][] prerequisites) {
         
-        ArrayList<Integer>[] graph=new ArrayList[n];
+        ArrayList<Integer>[] graph = new ArrayList[n];
         
-        for(int i=0;i<n;i++)
+         for(int i=0;i<n;i++)
         {
             graph[i]=new ArrayList<>();
         }
-        
-        for(int[] arr: prerequisites)
+        for(int[] arr : prerequisites)
         {
             graph[arr[0]].add(arr[1]);
         }
         
-        return kahns(graph,n);
+        return Kahns(graph,n);
+        
+        
         
     }
 }
