519392f51f61bb9d33993d98ca3046459a04752c
Niharika Chhabra <72100217+NiharikaChhabra700@users.noreply.github.com>
2022-07-26 12:18:27 +0530

    Time: 17 ms (91.42%), Space: 58.5 MB (69.88%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,59 +1,46 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         
-        if(grid.length==0 || grid[0].length==0)
-        {
-            return -1;
-        }
+        int n = grid.length, m = grid[0].length;
         
-        int n=grid.length;
-        int m=grid[0].length;
+        if(grid[0][0]!=0 || grid[n-1][m-1] != 0) return -1;
         
-        if(grid[0][0] == 1 || grid[n-1][m-1] == 1)
-        {
-            return -1;
-            
-        }
+        int[][] dir = {{1,0},{0,1},{0,-1},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
         
-         int[][] dir={{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,1},{-1,-1},{1,-1}};
-        
-        
-        LinkedList<Integer> que=new LinkedList<>();
-        que.addLast(0);
-        int level=1;
+        LinkedList<Integer> que = new LinkedList<>();
+        que.addLast(0*m + 0);
+        int level = 1;
         
         while(que.size()!=0)
         {
-            int size=que.size();
-            
+            int size = que.size();
             while(size-- > 0)
             {
-                int idx=que.removeFirst();
+                int idx = que.removeFirst();
                 
-                int r=idx/m;
-                int c=idx%m;
+                int r = idx/m;
+                int c = idx%m;
                 
-                if(r == n-1 && c == m-1)
+                if(r== n-1 && c==m-1)
                 {
                     return level;
                 }
                 
-                for(int d=0;d<dir.length;d++)
+                for(int d = 0 ;d<dir.length; d++)
                 {
-                    int x=r+dir[d][0];
-                    int y=c+dir[d][1];
+                    int x = r + dir[d][0];
+                    int y = c + dir[d][1];
                     
                     if(x>=0 && y>=0 && x<n && y<m && grid[x][y] == 0)
                     {
-                        grid[x][y]=1;
-                        que.addLast(x*m+y);
+                        grid[x][y] = 1;
+                        que.addLast(x*m + y);
                     }
                 }
-                
-                
             }
             
             level++;
+            
         }
         
         return -1;
